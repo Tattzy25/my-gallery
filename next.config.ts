f6f68cbc90f513be77b842a9e71bf98e9e0b51a0
Avi Kay <tattzy2025@gmail.com>
2026-08-01 11:59:45 -0700

    Modify image formats and add remote patterns
    Updated image formats in next.config.ts to include 'image/png' along with 'image/webp'. Added additional remote patterns for image sources.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/png", "image/webp"], // Note: Next.js only officially supports 'image/avif' and 'image/webp' here. If you get an error, you may need to remove 'image/png'.
     remotePatterns: [
       {
         protocol: "https",
@@ -13,6 +13,24 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "pix.tattty.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "imagine.tattty.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pvt.tattty.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-2a0cf56818694fa5b352a32e459c57f2.r2.dev",
         port: "",
         pathname: "/**",
       },
